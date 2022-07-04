b6a9791b0bc307dba87d61b08632973beca316aa
wendel <wendeldaniel263@gmail.com>
2022-07-04 15:40:19 -0300

    exercicio 4 e 5 ok

---
+++ script.js
@@ -0,0 +1,7 @@
+// const quadroP = document.getElementById('pixel-board');
+// quadroP.addEventListener('window.onload' function() {
+//    for (let i = 0; i < array.length; i++) {
+//     ;
+    
+//    } 
+// });
